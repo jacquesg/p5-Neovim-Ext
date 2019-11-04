82565d72a8659ebca9196cd672e44f6c7f436b01
Jacques Germishuys <jacquesg@striata.com>
2019-11-04 21:00:14 +0000

    Initial version

---
+++ lib/Neovim/Ext/RemoteApi.pm
@@ -0,0 +1,59 @@
+package Neovim::Ext::RemoteApi;
+
+use strict;
+use warnings;
+use base qw/Class::Accessor/;
+__PACKAGE__->mk_accessors (qw/nvim prefix/);
+
+
+sub new
+{
+	my ($this, $nvim, $prefix) = @_;
+
+	my $class = ref ($this) || $this;
+	my $self =
+	{
+		nvim => $nvim,
+		prefix => $prefix,
+	};
+
+	return bless $self, $class;
+}
+
+
+
+sub DESTROY
+{
+}
+
+
+our $AUTOLOAD;
+
+sub AUTOLOAD
+{
+	my $methodName;
+	($methodName = $AUTOLOAD) =~ s/.*:://;
+
+	# Install
+	no strict 'refs';
+	*{$AUTOLOAD} = sub
+	{
+		my $this = shift;
+		$this->nvim->request ($this->prefix.$methodName, @_);
+	};
+
+	goto &$AUTOLOAD;
+}
+
+=head1 NAME
+
+Neovim::Ext::RemoteApi - Neovim RemoteApi class
+
+=head1 SYNOPSIS
+
+	use Neovim::Ext;
+
+=cut
+
+1;
+
